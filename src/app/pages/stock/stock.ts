acc7550ebb1a1ab4b409718c83fe5da4e9956181
mlaurel19 <mauricio.laurel.19@gmail.com>
2025-10-29 23:49:09 -0400

    segundo commit

---
+++ src/app/pages/stock/stock.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock',
+  imports: [],
+  templateUrl: './stock.html',
+})
+export class Stock { }
